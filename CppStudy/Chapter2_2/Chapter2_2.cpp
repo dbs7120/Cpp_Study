8bc29256766b15c8082ab6e447efede0f13867f6
Adj <dbs7120@naver.com>
2020-06-12 23:43:32 +0900

    2020-06-12

---
+++ CppStudy/Chapter2_2/Chapter2_2.cpp
@@ -0,0 +1,89 @@
+﻿/*
+	인프런 홍정모의 따라하며 배우는 C++
+
+	*정수형
+	 char (최소크기: 1byte) / short (2) / int (2, 대부분은 4) / long (4) / long long (8)
+	 int i = 1;		<=>		00000000 | 00000000 | 00000000 | 00000001
+	 int i = -1;	<=>		10000000 | 00000000 | 00000000 | 00000001
+
+	*오버플로우(Overflow) 주의
+
+	*고정 너비 정수(Fixed-width Integers) C++ 11
+
+	*무치형(void) 타입
+*/
+
+#include <iostream>
+#include <cmath>
+#include <limits>		
+//#include <cstdint>	// <iostream> 에 포함됨 (int16_t, int8_t, int_fast8_t, int_least64_t)
+
+int main()
+{
+	using namespace std;
+
+	short s = 1;		// 2byte = 2*8bits = 16bits : 2^16가지 표현
+	int i = 1;
+	long l = 1;
+	long long ll = 1;
+	cout << sizeof(short) << " " << sizeof(int) << " " << sizeof(long) << " " << sizeof(long long) << endl;
+
+	cout << std::pow(2, sizeof(short) * 8 - 1) - 1 << endl;		// 부호있는(signed), 0제외의 short 형 표현수 
+	cout << std::numeric_limits<short>::max() << endl;			// limts 을 통해 자료형 최대 최소값 구하기
+	cout << std::numeric_limits<short>::min() << endl;
+	cout << std::numeric_limits<short>::lowest() << endl << endl;
+
+	s = 32767;
+	s += 1;		// (복합대입연산자) <=> s = s + 1 
+
+	cout << "max + 1: " << s << endl;	// Overflow
+
+	s = -32768;
+	s -= 1;
+	cout << "min - 1: " << s << endl;	// Overflow
+
+	/*----------------------------------------------------------------------------------*/
+
+	cout << std::numeric_limits<long long>::max() << endl;
+	cout << std::numeric_limits<long long>::lowest() << endl;
+
+	ll = std::numeric_limits<long long>::max();
+	ll += 1;
+	cout << "max + 1: " << ll << endl << endl;	// Overflow
+
+	unsigned int ui = -1;
+	cout << ui << endl;		// 이진수 표기때문에 Overeflow로 인해 unsigned int 최대값이 나옴
+	cout << std::numeric_limits<unsigned int>::max() << endl;
+	cout << std::numeric_limits<unsigned int>::lowest() << endl << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	cout << 22 / 4 << endl;					// 5, 정수의 연산 => 정수,소수점 손실(버림)
+	cout << (float)22 / 4 << endl << endl;	// 5.5, 실수/정수 => 실수값
+
+	/*----------------------------------------------------------------------------------*/
+
+	// 고정 너비 정수, 주로 멀티 플랫폼프로그래밍에서 활용
+	std::int16_t i16(5);
+	std::int8_t myint = 65;
+	std::int_fast64_t fi(65);
+	std::int_least64_t li(-1);
+
+	cout << myint << " " << sizeof(myint) << endl;
+	cout << fi << " " << sizeof(fi) << endl;
+	cout << li << " " << sizeof(li) << endl;
+	cout << std::numeric_limits<int_least64_t>::lowest() << endl << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	//	void vd					// void 는 메모리를 차지하지 않기 때문에 선언 불가
+	void *my_void;				// void타입 포인터형으로 선언
+	int num = 123;
+	float flo = 123.456f;
+
+	my_void = (void*)&num;
+	my_void = (void*)&flo;		// 데이터 타입이 다르고 사이즈가 다르더라도 데이터의 주소의 데이터 타입은 동일
+
+
+	return 0;
+}
---
+++ CppStudy/Chapter2_3/Chapter2_3.cpp
@@ -0,0 +1,124 @@
+﻿/*
+	인프런 홍정모의 따라하며 배우는 C++
+
+	*부동소수점 수(Floating Point Numbers)
+	 float (최소크기: 4byte) / double (8) / long double (8)	// double은 float의 두배 공간
+	 연산 속도 공간측면에서 float형으로 많이 사용
+	 부호부(sign) 지수(exponent) 가수(mantissa)로 구성됨
+
+	*불리언(Boolean), 조건문 if
+	 Bool ==> true(1) or flase(0)
+	 조건문 조건식 0이아닌 값 ==> true
+
+*/
+
+#include <iostream>
+#include <limits>
+#include <iomanip>		// 입출력을 제어 (소수점 자릿수등)
+#include <cmath>
+
+bool isEqual(int a, int b) 
+{
+	bool result = (a == b);
+	return result;
+}
+
+int main()
+{
+	using namespace std;
+
+	float f(3.141592f);
+	double d(3.141592f);
+	long double ld(3.141592f);
+
+	cout << 3.14 << endl;		// 3.14 = 31.4 * 0.1
+	cout << 31.4e-1 << endl;	// 지수 표기법
+	cout << 31.4e2 << endl << endl;
+
+	cout << sizeof(float) << " " << sizeof(double) << " " << sizeof(long double) << endl;
+	cout << numeric_limits<float>::max() << ", " << numeric_limits<float>::min() << ", " << numeric_limits<float>::lowest() << endl;		// min(): 표현할 수 있는 가장 작은 절대 값
+	cout << numeric_limits<double>::max() << ", " << numeric_limits<double>::min() << ", " << numeric_limits<double>::lowest() << endl;
+	cout << numeric_limits<long double>::max() << ", " << numeric_limits<long double>::min() << ", " << numeric_limits<long double>::lowest() << endl << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	f = 123456789.0f;
+	d = 0.1;
+	double d1 = 1.0;
+	double d2 = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+
+	cout << 1.0 / 3.0 << endl;
+	cout << f << endl;
+	cout << d << endl;
+	cout << d1 << " equals " << d2 << endl << endl;
+
+	cout << std::setprecision(16);				// cout의 소수점 16 지정
+	cout << 1.0 / 3.0 << endl;
+	cout << f << endl;							// 정밀도 손실
+	cout << d << endl;
+	cout << d1 << " equals " << d2 << endl << endl;		// 오차의 누적
+
+	/*----------------------------------------------------------------------------------*/
+
+	double zero = 0.0;
+	double posinf = 5.0 / zero;
+	double neginf = -5.0 / zero;
+	double nan = zero / zero;		// Not a Number
+	cout << "1: " << posinf << " 2: " << neginf << " 3: " << nan << endl << endl;		// inf , -inf , -nan(ind) ==> 디버깅시의 문제 isnan, isinf 함수로 확인
+
+	// std::isnan() 숫자가 아닌 값인지 확인, std::isinf() inf값 인지 확인
+	cout << neginf << " " << std::isinf(neginf) << endl;
+	cout << nan << " " << std::isnan(nan) << endl;
+	cout << 1.0 << " " << std::isnan(1.0) << endl << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	bool b1 = true;		// true = 1
+	bool b2(false);	// false = 0
+	bool b3{ true };
+
+	cout << b1 << " " << b2 << " " << b3 << endl;
+	cout << std::boolalpha;								// cout 1=>true 0=>false로 출력
+	cout << b1 << " " << b2 << " " << b3 << endl;
+	cout << std::noboolalpha;
+	cout << !b1 << endl << endl;
+
+	cout << (true && true) << " " << (true && false) << " " << (false && true) << " " << (false && false) << endl;		// &&(and) 논리 연산
+	cout << (true || true) << " " << (true || false) << " " << (false || true) << " " << (false || false) << endl;		// ||(or) 논리 연산
+
+	/**/
+	int num = 10;
+	cout << (true || (3 == 1 ? num = 1 : num = 2)) << endl;		// 논리연산 단축평가
+	cout << num << endl;
+	cout << (false && (3 == 1 ? num = 1 : num = 0)) << endl;	// 논리연산 단축평가
+	cout << num << endl << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	// if문
+	if (3 > 1)	// 조건식
+		cout << "This is true" << endl;				// 조건식이 참일경우 실행
+	else
+		cout << "This is false" << endl << endl;	// 조건식이 참일경우 실행 X
+
+	if ('a')										// 0이 아닌값은 True
+		cout << "a is true " << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	cout << std::boolalpha;
+	cout << isEqual(1, 1) << endl;
+	cout << isEqual(3, 4) << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	int input;	
+	cout << "Your input: ";
+	cin >> input; 
+	if (input % 2 == 0)
+		cout << input << "is Even number" << endl;
+	else
+		cout << input << "is Odd number" << endl;
+
+	return 0;
+}
