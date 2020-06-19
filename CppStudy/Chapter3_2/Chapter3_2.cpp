575cd353b0a875bda433ce0ecf6e27035d8566dd
Adj <dbs7120@naver.com>
2020-06-19 22:07:22 +0900

    2020-06-19

---
+++ CppStudy/Chapter3_2/Chapter3_2.cpp
@@ -0,0 +1,133 @@
+﻿/*
+	인프런 홍정모의 따라하며 배우는 C++
+
+	◆관계연산자(Relational Operators)
+	 >, >=, <, <=, ==, !=
+	 부동소수점에서의 값비교 주의(수치해석)
+
+	◆논리연산자(Logical Operators)
+	 &&(and), ||(or), !(not)
+	 가능하면 명확하고 단순하게 표현
+	 논리연산자 사이의 우선순위: ! > && > ||
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	using namespace std;
+
+	int x, y;
+	cin >> x >> y;
+	cout << "Your input values are : " << x << " " << y << endl;
+
+	if (x == y)	cout << "equal" << endl;
+	if (x != y)	cout << "Not equal" << endl;
+	if (x > y)	cout << "x is greater than y" << endl;
+	if (x < y)	cout << "x is less than y" << endl;
+	if (x >= y)	cout << "x is greater than y or equal to y" << endl;
+	if (x <= y)	cout << "x is less than or equal to y" << endl;
+
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	// 부동소수점에서의 비교 주의
+
+	double d1(100 - 99.99);		// 0.001
+	double d2(10 - 9.99);		// 0.001
+	cout << d1 << " " << d2 << endl;
+
+	if (d1 == d2)
+		cout << "d1==d2" << endl;
+	else
+	{
+		cout << "d1!=d2" << endl;
+
+		if (d1 > d2) cout << "d1 > d2" << endl;
+		else cout << "d1 < d2" << endl;				// if(d1 < d2), 상위비교에서 d1 != d2 검증
+	}
+
+	cout << std::abs(d1 - d2) << endl << endl;		// d1과 d2차이(절대값)
+
+	const double epsilon = 1e-10;					// 오차한계지정	==> 수치는 분야, 환경에 따라 달리지정
+
+	if (std::abs(d1 - d2) < epsilon)				// 오차한계 직접지정
+		cout << "d1, d2 is Approximately equal" << endl;
+	else
+		cout << "d1, d2 is not equal" << endl;
+
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	// 논리연산자 활용
+
+	bool hit = true;
+	int health = 10;
+
+	if (hit == true && health < 20)						// <== if(hit == true) if(health < 20)
+		cout << "die " << endl;
+	else
+		health -= 20;
+	cout << endl;
+
+	int a = 5;
+	int b = 7;
+	if (!a == b) cout << "a does not equal b" << endl;	// 연산자우선순위 !가 == 보다 높음 ((!a)==b) ==> if(!(a == b)) == if(a != b)
+	else cout << "a equals b" << endl;
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	// short circuit evaluation (단축연산)
+	x = 1, y = 2;
+
+	if (x == 1 && y++ == 2)
+	{
+		// do something
+	}
+	cout << "first" << y << endl;
+
+	x = 2, y = 2;
+
+	if (x == 1 && y++ == 2)				// &&: 좌측항이 false값이되면 우측항무시하고 결과 리턴
+	{
+		// do something
+	}
+	cout << "second" << y << endl;
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	bool v1 = true;
+	bool v2 = false;
+	bool v3 = false;
+
+	bool r1 = v1 || v2 && v3;			// &&가 || 보다 우선순위 높음(r1==r3)
+	bool r2 = (v1 || v2) && v3;			// 괄호를 통해 코드명확하게 하기
+	bool r3 = v1 || (v2 && v3);
+	cout << "r1: " << r1 << " r2: " << r2 << " r3: " << r3 << endl << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	// De Morgan's Law
+	// x = 1, y = 0;
+	// !(x && y); <==> !x || !y;
+	// !(x || y); <==> !x && !y;
+
+	// XOR	==> if(x != y), x,y는 bool타입만
+	// TT=>F, TF=>T, FT=>T, FF=>F
+
+	/*----------------------------------------------------------------------------------*/
+
+	//ex
+	cout << ((true && true) || false) << endl;				// T
+	cout << ((false && true) || true) << endl;				// T
+	cout << ((false && true) || false || true) << endl;		// T
+	cout << ((14 > 13 || 2 > 1) && (9 > 1)) << endl;		// T
+	cout << (!(2314123 > 2 || 123123 > 2387)) << endl;		// F
+
+	return 0;
+}
---
+++ CppStudy/Chapter3_3/Chapter3_3.cpp
@@ -0,0 +1,186 @@
+﻿/*
+	인프런 홍정모의 따라하며 배우는 C++
+
+	◆이진수(Binary Numbers)
+
+	◆비트단위 연산자(Bitwise Operators)
+	 ~, &, ^, |, <<, >>
+	 2진수 각자리에대한 연산, 계산속도가 빠름
+
+	◆비트 플래그, 비트마스크(Bit flags, Bit masks) 활용
+
+*/
+
+#include <iostream>
+#include <bitset>	// 2진수 활용 라이브러리
+
+int main()
+{
+	using namespace std;
+
+	/*
+	10진법	337 = 300 + 30 + 7 = 10^2 * 3 + 10^1 * 3 + 10^0 * 7
+	2진법	10 = 2^1 * 1 + 2^0 * 0
+
+		0000 0101 <=> 5
+	보수 complement
+		0000 0101 -> 1111 1010
+	보수 + 1(숫자 +0,-0 방지)
+		1111 1010 + 1 -> 1111 1010 <=> -5
+
+	// signed vs unsigned
+		1001 1110 -> 2^7*1 + 2^4*1 + 2^3*1+1 + 2^2*1+1 + 2^1*1+1 = 128+16+8+4+2=158
+	*/
+
+	/*----------------------------------------------------------------------------------*/
+
+	// << : left shift, >> : right shift		(cout의 <<, cin의 >> 와 다른용도(오버로딩됨))
+	// ~ : not, & : and, | : or, ^ : xor
+	// 일반적으로 unsigned 2진수로 연산
+
+	unsigned int a = 3;
+	cout << std::bitset<8>(a) << endl;			// 정수 a를 <size>자리의 2진법으로 출력
+
+	unsigned int b = a << 3;					// a를 왼쪽으로 n칸 옮김(left shift)  10진수로 2의 n승 곱 효과
+	cout << std::bitset<8>(b) << endl;
+
+	cout << a << " " << b << endl << endl;
+
+	a = 1024;
+	cout << std::bitset<16>(a) << endl;
+	b = a >> 4;									// 10진수로 2의 n승 나눈 결과
+	cout << std::bitset<16>(b) << endl;
+	cout << a << " " << b << endl;
+
+	cout << std::bitset<16>(a) << " " << (a) << " " << std::bitset<16>(~a) << " " << (~a) << endl << endl;	// bit not (~)
+
+	a = 0b1100;
+	b = 0b0110;
+
+	cout << std::bitset<4>(a & b) << endl;		// bitwise AND
+	cout << std::bitset<4>(a | b) << endl;		// bitwise OR
+	cout << std::bitset<4>(a ^ b) << endl;		// bitwise XOR
+
+	a &= b; a ^= b; a |= b;						// 복합대입형태로 사용가능
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	//ex
+	cout << (0b0110 >> 2) << endl;
+	cout << (5 | 12) << endl;		// 0101 | 1100 => 1101
+	cout << (5 & 12) << endl;		// 0101 & 1100 => 0100
+	cout << (5 ^ 12) << endl;		// 0101 ^ 1100 => 1001 
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	/*
+	bool item1_flag = false;
+	bool item2_flag = false;
+	bool item3_flag = false;
+	...
+	bool item8_flag = false;
+	==>
+	*/
+
+	// byte구조를활용(8bit) 시나리오
+	const unsigned char opt0 = 1 << 0;
+	const unsigned char opt1 = 1 << 1;
+	const unsigned char opt2 = 1 << 2;
+	const unsigned char opt3 = 1 << 3;
+	// ...opt4, 5, 6, 7
+
+	cout << bitset<8>(opt0) << endl;
+	cout << bitset<8>(opt1) << endl;
+	cout << bitset<8>(opt2) << endl;
+	cout << bitset<8>(opt3) << endl;
+	cout << endl;
+
+	unsigned char items_flag = 0;									// 1byte = 8bit
+	cout << "No item \t\t" << bitset<8>(items_flag) << endl;
+
+	// item 0 on
+	items_flag |= opt0;
+	cout << "Item0 obtained \t\t" << bitset<8>(items_flag) << endl;
+
+	// item 3 on
+	items_flag |= opt3;
+	cout << "Item3 obtained \t\t" << bitset<8>(items_flag) << endl;
+
+	// item 3 lost
+	items_flag &= ~opt3;
+	cout << "Item3 lost \t\t" << bitset<8>(items_flag) << endl;
+
+	// item 1 check
+	if (items_flag & opt1) { cout << "Item1 exist \t\t" << bitset<8>(items_flag) << endl; }
+	else { cout << "Item1 not exist \t" << bitset<8>(items_flag) << endl; }
+
+	// item 0 check
+	if (items_flag & opt0) { cout << "Item0 exist \t\t" << bitset<8>(items_flag) << endl; }
+	else { cout << "Item0 not exist \t\t" << bitset<8>(items_flag) << endl; }
+
+	// item 2,3 on
+	items_flag |= (opt2 | opt3);
+	cout << "Item2,3 obtained \t" << bitset<8>(items_flag) << endl;
+
+	// item 1 x->o, item 2 o->x
+	if ((items_flag & opt2) && !(items_flag &opt1))
+	{
+		items_flag ^= opt2;
+		items_flag ^= opt1;
+	}
+	cout << "Item1,2 exchange \t" << bitset<8>(items_flag) << endl;
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	// glCler(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)	// API디자인 옵션 => bitflag사용(|)
+
+	// rgb == 3byte(255,255,255)
+	// rgb 추출
+	const unsigned int red_mask = 0xFF0000;					// mask: 해당구역 지정
+	const unsigned int green_mask = 0x00FF00;
+	const unsigned int blue_mask = 0x0000FF;
+
+	unsigned int pixel_color = 0xDAA520;
+	cout << std::bitset<32>(pixel_color) << endl;
+
+	unsigned int red, green, blue;
+	red = (pixel_color & red_mask) >> 16;				// 기존자리 옮기기(shift)
+	green = (pixel_color & green_mask) >> 8;		
+	blue = pixel_color & blue_mask;
+
+	cout << "red\t" << bitset<8>(red) << " " << int(red) << endl;
+	cout << "green\t" << bitset<8>(green) << " " << int(green) << endl;
+	cout << "blue\t" << bitset<8>(blue) << " " << int(blue) << endl;
+	cout << endl;
+
+	/*----------------------------------------------------------------------------------*/
+
+	//ex
+	unsigned char option_viewed = 0x01;
+	unsigned char option_edited = 0x02;
+	unsigned char option_liked = 0x04;
+	unsigned char option_shared = 0x08;
+	unsigned char option_deleted = 0x80;
+
+	unsigned char my_article_flags = 0;
+	cout << "Init: \t\t" << bitset<8>(my_article_flags) << endl;
+
+	// 기사를 봤을때
+	my_article_flags |= option_viewed;
+	cout << "Viewed: \t" << bitset<8>(my_article_flags) << endl;
+	// 기사의 좋아요를 클릭
+	my_article_flags |= option_liked;
+	cout << "Liked: \t\t" << bitset<8>(my_article_flags) << endl;
+	// 기사의 좋아요를 다시 클릭
+	my_article_flags &= ~option_liked;
+	cout << "Cancled: \t" << bitset<8>(my_article_flags) << endl;
+	// 본기사만 삭제
+	if(my_article_flags & option_viewed)			// Viewd 값 1이면
+		my_article_flags |= option_deleted;			// 해당값 or
+	cout << "Deleted: \t" << bitset<8>(my_article_flags) << endl;
+
+	return 0;
+}
